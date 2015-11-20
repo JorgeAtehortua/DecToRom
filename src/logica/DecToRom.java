27850cc35be43e0bae9938496885f916a843db0c
JorgeAtehortua <jorge.atehortua@gmail.com>
2015-11-19 23:13:14 -0500

    commit inicial con los archivos base para la aplicacion

---
+++ src/logica/DecToRom.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package logica;
+
+/**
+ * @author	Jorge Atehortua
+ * @email 	jorge.atehortua@gmail.com
+ */
+public class DecToRom {
+
+}
---
+++ test/test/DecToRomTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author	Jorge Atehortua
+ * @email 	jorge.atehortua@gmail.com
+ */
+public class DecToRomTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
